4e9c3c2d63be09120681ec9e6fa42089470becf0
Md. Monowarul Islam <monowar.mbstu@gmail.com>
2016-06-23 11:59:39 +0600

    Project Added

---
+++ BluetoothTestApp/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace BluetoothTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(156, 46);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(156, 86);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(254, 46);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send file";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(124, 173);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(254, 86);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(75, 23);
+            this.btnReceive.TabIndex = 6;
+            this.btnReceive.Text = "Receive File";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 214);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BluetoothTestApp/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using InTheHand.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Security.Permissions;
+
+namespace BluetoothTestApp
+{
+    public partial class Form1 : Form
+    {
+        BluetoothClient bluetoothClient;
+        BluetoothDeviceInfo[] deviceList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (BluetoothRadio.IsSupported)
+                {
+                    bluetoothClient = new BluetoothClient();
+
+                    if (BluetoothRadio.PrimaryRadio.Mode == RadioMode.PowerOff)
+                        BluetoothRadio.PrimaryRadio.Mode = RadioMode.Connectable;
+                }
+                else
+                {
+                    MessageBox.Show("Adaptar is not detected");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sbdd = new SelectBluetoothDeviceDialog();
+                sbdd.ShowAuthenticated = true;
+                sbdd.ShowRemembered = true;
+                sbdd.ShowUnknown = true;
+
+                BluetoothDeviceInfo deviceInfo = null;
+
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+
+                if (sbdd.ShowDialog() == DialogResult.OK)
+                {
+                    deviceInfo = sbdd.SelectedDevice;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        var file = openFileDialog.FileName;
+                        var uri = new Uri("obex://" + deviceInfo.DeviceAddress + "/" + file);
+                        var request = new ObexWebRequest(uri);
+                        request.ReadFile(file);
+                        var response = (ObexWebResponse)request.GetResponse();
+                        MessageBox.Show(response.StatusCode.ToString());
+                        response.Close();
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ObexListener listener = new ObexListener();
+                listener.Start();
+
+                ObexListenerContext context = null;
+                frmWaitingConnection waitingConnectionForm = new frmWaitingConnection();
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+                new Thread(() =>
+                {
+                    Thread.CurrentThread.IsBackground = true;
+                    context = listener.GetContext();
+                    if (waitingConnectionForm.Visible)
+                        waitingConnectionForm.Invoke(new MethodInvoker(delegate { waitingConnectionForm.Close(); }));
+                }).Start();
+
+                if (waitingConnectionForm.ShowDialog() == DialogResult.Cancel)
+                {
+                    if (listener.IsListening)
+                        listener.Stop();
+                }
+
+                if (context != null)
+                {
+                    if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string fileLocation = folderBrowserDialog.SelectedPath + context.Request.RawUrl;
+                        context.Request.WriteFile(fileLocation);
+                        MessageBox.Show("File saved at: \n" + fileLocation);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                deviceList = bluetoothClient.DiscoverDevices();
+
+                listBox1.DataSource = deviceList;
+                listBox1.DisplayMember = "DeviceName";
+                listBox1.ValueMember = "DeviceAddress";          
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EventHandler<BluetoothWin32AuthenticationEventArgs> handler = new EventHandler<BluetoothWin32AuthenticationEventArgs>(HandleRequests);
+                BluetoothWin32Authentication auth = new BluetoothWin32Authentication(handler);
+
+                if (BluetoothSecurity.PairRequest((BluetoothAddress)listBox1.SelectedValue, null))
+                {
+                    MessageBox.Show("Connected");
+                }
+                else
+                {
+                    MessageBox.Show("Unable to connect");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void HandleRequests(object that, BluetoothWin32AuthenticationEventArgs e)
+        {
+            e.Confirm = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
